82adff78c5458b7b97b71157faea6502f2c32ea7
Alan Morales <alan.morales@wexford.edu.mx>
2026-02-17 08:13:15 -0600

    Creacion de mas usuarios

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -21,5 +21,53 @@ class AdminUserSeeder extends Seeder
                 'password' => Hash::make('admin123'),
             ]
         );
+
+        User::firstOrCreate(
+            ['email' => 'ruben.rodriguez@wexford.edu.mx'],
+            [
+                'name' => 'Ruben',
+                'password' => Hash::make('ruben123'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'martin.arreola@wexford.edu.mx'],
+            [
+                'name' => 'Martin',
+                'password' => Hash::make('martin123'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'josafat.lopez@wexford.edu.mx'],
+            [
+                'name' => 'Angel',
+                'password' => Hash::make('angel123'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'alberto.pimentel@wexford.edu.mx'],
+            [
+                'name' => 'Alberto',
+                'password' => Hash::make('albertos123'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'alan.morales@wexford.edu.mx'],
+            [
+                'name' => 'Alan',
+                'password' => Hash::make('alan123'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'joel.juarez@wexford.edu.mx'],
+            [
+                'name' => 'Joel',
+                'password' => Hash::make('joel123'),
+            ]
+        );
     }
 }
